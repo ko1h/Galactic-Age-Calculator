044c1e0574abac73eed966c9017872a124233e68
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-15 16:31:50 -0700

    working on front end logic to work with backend logic

--- galactic-calculator/src/index.js
+++ galactic-calculator/src/index.js
@@ -1,19 +1,21 @@
 import 'bootstrap';
-// import $ from 'jquery';
+import $ from 'jquery';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
 // import { AgeCalendar } from './galactic-calculator.js';
 
 
-// $(document).ready(function() {
-//   $("#age-form").submit(function(event) {
-//     event.preventDefault();
-//     let dateInput = $('#birthday').val();
-//     let sexInput = $('#space-gender').val();
-//     let newAgeCalculator = new AgeCalculator (dateInput, sexInput);
-//     let birthdayInput = new Date(dateInput);
-//
-//
-//   });
-//
-// });
+$(document).ready(function() {
+  $('#age-form').submit(function(event) {
+    event.preventDefault();
+    var dateInput = $('input#birthday').val();
+    // let sexInput = $('select#space-gender').val();
+    // let newAgeCalculator = new AgeCalculator (dateInput);
+    var today = dateInput.getDay();
+    // let birthdayInput = new Date(dateInput);
+    console.log(today);
+
+
+  });
+
+});
