f921147f313488ef86d6c3959c3b118f150e4db4
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-20 20:20:33 -0700

    Add UI and frontend logic, to get user info

--- galactic-calculator/src/index.js
+++ galactic-calculator/src/index.js
@@ -2,17 +2,16 @@ import 'bootstrap';
 // import $ from 'jquery';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
-// import { AgeCalendar } from './galactic-calculator.js';
-//
-//
-// $(document).ready(function() {
-//   $('#age-form').submit(function(event) {
-//     event.preventDefault();
-//     let dateInput = $('input#birthday').val();
-//     let birthday = new Date
-//     let today = new Date();
-//
-//     let ageCalculatorOutput = new AgeCalculator(birthday, today);
-//
-//
-//   });
+import { User } from './business.js';
+
+
+$(document).ready(function() {
+  $('#age-form').submit(function(event) {
+    event.preventDefault();
+    let ageInput = $('input#age').val();
+    let sexInput = $('select#sex').val();
+
+    let ageCalculatorOutput = new User(ageInput, sexInput);
+
+
+  });
