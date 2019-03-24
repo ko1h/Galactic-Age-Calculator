d6a89761b5a233fc3458b379717392d8555685ab
Kohichi Horii <kohichihorii@Kohichis-MacBook-Pro.local>
2019-03-24 15:02:16 -0700

    fixing issue with program not compiling

---
+++ spec/galactic-calculator-spec.js
@@ -0,0 +1,28 @@
+import { User } from './../src/business.js';
+
+describe ('User', function() {
+  const age = new User(30);
+
+  const sex = new User(30, 5, 80);
+
+
+
+  it("Should calculate a person age on mercury", function() {
+    expect(age.lifeOnMercury()).toEqual(124);
+  });
+  it("Should calculate a person age on venus", function() {
+    expect(age.lifeOnVenus()).toEqual(48);
+  });
+  it("Should calculate a person age on mars", function() {
+    expect(age.lifeOnMars()).toEqual(15);
+  });
+  it('should subtract average life expectany to how long you live on Venus', function() {
+    expect(sex.lifeExpectancyOnVenus()).toEqual(37);
+  });
+  it('should subtract average life expectany to how long you live on Mars', function() {
+    expect(sex.lifeExpectancyOnMars()).toEqual(37);
+  });
+  it('should subtract average life expectany to how long you live on Jupiter', function() {
+    expect(sex.lifeExpectancyOnJupiter()).toEqual(37);
+  });
+});
---
+++ src/business.js
@@ -0,0 +1,41 @@
+export class User {
+
+  constructor(age, sex, averageAge) {
+    this.age= age;
+    this.sex = sex;
+    this.averageAge = averageAge;
+  }
+
+  lifeOnMercury() {
+    const ageMercury = Math.floor((this.age * 365) / 88);
+    return ageMercury;
+  }
+  lifeOnVenus() {
+    const ageVenus = Math.floor((this.age * 365) / 226);
+    return ageVenus;
+  }
+  lifeOnMars() {
+    const ageMars = Math.floor((this.age * 365) / 686);
+    return ageMars;
+  }
+  lifeOnJupiter() {
+    const ageJupiter = Math.floor((this.age * 365) / 4328);
+    return ageJupiter;
+  }
+  lifeExpectancyOnVenus() {
+    const yearleftOnVenus = (this.averageAge + this.sex) - Math.floor((this.age * 365) / 226);
+    return yearleftOnVenus;
+  }
+  lifeExpectancyOnMars() {
+    const yearleftOnMars = (this.averageAge + this.sex) - Math.floor((this.age * 365) / 226);
+    return yearleftOnMars;
+  }
+  lifeExpectancyOnJupiter() {
+    const yearleftOnJupiter = (this.averageAge + this.sex) - Math.floor((this.age * 365) / 226);
+    return yearleftOnJupiter;
+  }
+}
+
+
+//2640
+//10950
